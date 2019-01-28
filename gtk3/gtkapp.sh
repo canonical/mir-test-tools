8b0ff720e0b342f3a76d5eb4a93d016facb811c4
Alan Griffiths <alan@octopull.co.uk>
2019-01-28 11:24:35 +0000

    gnome-sudoku is simpler than gnome-chess

--- gtk3/gtkapp.sh
+++ gtk3/gtkapp.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-if [ ! -f "$XDG_CONFIG_HOME/gnome-chess/engines.conf" ]; then
-    mkdir -p $XDG_CONFIG_HOME/gnome-chess
-    cp $SNAP/etc/gnome-chess/engines.conf $XDG_CONFIG_HOME/gnome-chess
-fi
-
-exec desktop-launch gnome-chess&
+exec desktop-launch gnome-sudoku&
 pid=$!
 trap 'kill -s SIGTERM $pid && exit 0' SIGTERM
 wait $pid
