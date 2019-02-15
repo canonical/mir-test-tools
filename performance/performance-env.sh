16c5ed14ae4a08731f8c3f078107d6f73fb83597
Alan Griffiths <alan@octopull.co.uk>
2019-02-15 16:15:06 +0100

    Correct approach to setting up env for performance tests

--- performance/performance-env.sh
+++ performance/performance-env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-MIR_SERVER_VT=$(snapctl get vt)
-MIR_SERVER_CONSOLE_PROVIDER=$(snapctl get console-provider)
+export MIR_SERVER_VT=$(snapctl get vt)
+export MIR_SERVER_CONSOLE_PROVIDER=$(snapctl get console-provider)
 # Hack to workaround issue #704
 if [[ $(uname -r) =~ ^.*raspi2$ ]]; then
-  MIR_MESA_KMS_DISABLE_MODESET_PROBE=on
+  export MIR_MESA_KMS_DISABLE_MODESET_PROBE=on
 fi
 exec "$@"
