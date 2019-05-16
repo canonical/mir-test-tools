2c387d2cf57024ee8387943ccc95caa1ab0b25a9
Alan Griffiths <alan@octopull.co.uk>
2019-05-16 09:59:28 +0200

    Enable running the tests on the X11 desktop

--- performance/performance-env.sh
+++ performance/performance-env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-export MIR_SERVER_VT=$(snapctl get vt)
-export MIR_SERVER_CONSOLE_PROVIDER=$(snapctl get console-provider)
+if [ $(id -u) == 0 ]; then
+  export MIR_SERVER_VT=$(snapctl get vt)
+  export MIR_SERVER_CONSOLE_PROVIDER=$(snapctl get console-provider)
+fi
 # Hack to workaround issue #704
 if [[ $(uname -r) =~ ^.*raspi2$ ]]; then
   export MIR_MESA_KMS_DISABLE_MODESET_PROBE=on
