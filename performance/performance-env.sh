e8eedc5a93dc7e157f750a8e6fafb3b9281ea796
Alan Griffiths <alan@octopull.co.uk>
2020-06-04 15:16:08 +0100

    Use bash when there are bashisms

--- performance/performance-env.sh
+++ performance/performance-env.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 if [ $(id -u) == 0 ]; then
   export MIR_SERVER_VT=$(snapctl get vt)
   export MIR_SERVER_CONSOLE_PROVIDER=$(snapctl get console-provider)
 fi
 # Hack to workaround issue #704
-if [ $(uname -r) =~ ^.*raspi2$ ]; then
+if [[ $(uname -r) =~ ^.*raspi2$ ]]; then
   export MIR_MESA_KMS_DISABLE_MODESET_PROBE=on
 fi
 
