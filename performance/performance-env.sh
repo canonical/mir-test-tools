e5bac83f2846a0d1bf3f3b642544e9825dcf3d33
Alan Griffiths <alan@octopull.co.uk>
2020-06-04 10:11:07 +0100

    Enable the X11 performance tests for Mir 2.0

--- performance/performance-env.sh
+++ performance/performance-env.sh
@@ -4,7 +4,25 @@ if [ $(id -u) == 0 ]; then
   export MIR_SERVER_CONSOLE_PROVIDER=$(snapctl get console-provider)
 fi
 # Hack to workaround issue #704
-if [[ $(uname -r) =~ ^.*raspi2$ ]]; then
+if [ $(uname -r) =~ ^.*raspi2$ ]; then
   export MIR_MESA_KMS_DISABLE_MODESET_PROBE=on
 fi
+
+# For X11 we need .X11-unix/ in the snap's /tmp directory
+mkdir -p /tmp/.X11-unix
+
+# We need to stop Mir conflicting with any existing X11 session.
+# Mir detects existing sessions my scanning /tmp/.X11-unix
+# but we don't have the real /tmp, but a snap specific one.
+# So fake the existing $DISPLAY in the fake /tmp/.X11-unix
+if [ -z "${DISPLAY}" ]
+then touch /tmp/.X11-unix/X${DISPLAY:1}
+fi
+# A few more for luck
+touch /tmp/.X11-unix/X0
+touch /tmp/.X11-unix/X1
+touch /tmp/.X11-unix/X2
+touch /tmp/.X11-unix/X3
+touch /tmp/.X11-unix/X4
+
 exec "$@"
