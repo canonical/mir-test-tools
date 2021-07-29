9843500ab34046dde6c39fe8d763fc83982b1a45
Alan Griffiths <alan@octopull.co.uk>
2021-07-29 10:04:29 +0100

    We can no longer run `Xwayland` when the `x11` plug is connected. So:
     1. Drop the hack to support that; and,
     2. Default to suppressing `Xwayland` tests when x11 is connected.

--- performance/performance-env.sh
+++ performance/performance-env.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-# For X11 we need .X11-unix/ in the snap's /tmp directory
-mkdir -p /tmp/.X11-unix
-
-# We need to stop Mir conflicting with any existing X11 session.
-# Mir detects existing sessions my scanning /tmp/.X11-unix
-# but we don't have the real /tmp, but a snap specific one.
-# So fake the existing $DISPLAY in the fake /tmp/.X11-unix
-if [ -z "${DISPLAY}" ]
-then touch /tmp/.X11-unix/X${DISPLAY:1}
+if ! snapctl is-connected x11 || echo "$@" | grep --quiet "\-\-gtest_filter="; then
+  exec "$@"
+else
+  exec "$@" --gtest_filter=-GLMark2Xwayland.*
 fi
-# A few more for luck
-touch /tmp/.X11-unix/X0
-touch /tmp/.X11-unix/X1
-touch /tmp/.X11-unix/X2
-touch /tmp/.X11-unix/X3
-touch /tmp/.X11-unix/X4
-
-exec "$@"
