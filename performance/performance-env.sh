ad7befe2eebf5948d05f4223c5ab1c6e4c017f4c
Alan Griffiths <alan@octopull.co.uk>
2021-07-29 11:49:05 +0100

    We still need to create /tmp/.X11-unix

--- performance/performance-env.sh
+++ performance/performance-env.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# For X11 we need .X11-unix/ in the snap's /tmp directory
+mkdir -p /tmp/.X11-unix
+
 if ! snapctl is-connected x11 || echo "$@" | grep --quiet "\-\-gtest_filter="; then
   exec "$@"
 else
