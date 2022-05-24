4e4484f786ff72d673c13f934e5e14768440cd41
Alan Griffiths <alan@octopull.co.uk>
2022-05-24 09:47:53 +0100

    Don't screw with `snap run --shell` commandline

--- performance/performance-env.sh
+++ performance/performance-env.sh
@@ -3,7 +3,7 @@
 # For X11 we need .X11-unix/ in the snap's /tmp directory
 mkdir -p /tmp/.X11-unix
 
-if ! snapctl is-connected x11 || echo "$@" | grep --quiet "\-\-gtest_filter="; then
+if ! snapctl is-connected x11 || echo "$@" | grep --quiet -e "\-\-gtest_filter=" -e "/bin/bash"; then
   exec "$@"
 else
   exec "$@" --gtest_filter=-GLMark2Xwayland.*
