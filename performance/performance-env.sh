1c417009c19d3eaeeacb163c2c4c385da3e07dfe
Michał Sawicz <michal@sawicz.net>
2023-03-30 14:24:21 +0200

    performance: don't hide x11 tests
    Rather than hiding them, require the test environment to be right, or disable the tests.

--- performance/performance-env.sh
+++ performance/performance-env.sh
@@ -3,8 +3,4 @@
 # For X11 we need .X11-unix/ in the snap's /tmp directory
 mkdir -p /tmp/.X11-unix
 
-if ! snapctl is-connected x11 || echo "$@" | grep --quiet -e "\-\-gtest_filter=" -e "/bin/bash"; then
-  exec "$@"
-else
-  exec "$@" --gtest_filter=-GLMark2Xwayland.*
-fi
+exec "$@"
