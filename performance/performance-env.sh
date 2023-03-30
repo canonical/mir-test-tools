0579f7b85b18862f97d9d17c28f919283025407a
Alan Griffiths <alan@octopull.co.uk>
2023-03-30 16:01:09 +0100

    Workaround existing X11

--- performance/performance-env.sh
+++ performance/performance-env.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# For X11 we need .X11-unix/ in the snap's /tmp directory
-mkdir -p /tmp/.X11-unix
+if ! snapctl is-connected x11; then
+  # For X11 we need .X11-unix/ in the snap's /tmp directory
+  mkdir -p /tmp/.X11-unix
+  # We also need to avoid any sockets already used by the system
+  touch /tmp/.X11-unix/X{0,1,2}
+fi
 
 if ! snapctl is-connected x11 || echo "$@" | grep --quiet -e "\-\-gtest_filter=" -e "/bin/bash"; then
   exec "$@"
